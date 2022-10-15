3948430644936836c145f0e52d68b1f2a0da763c
Casual Garage Coder <contact@casualgaragecoder.com>
2022-10-16 00:30:08 +0200

    Added timer management

--- control_fsm.gd
+++ control_fsm.gd
@@ -23,6 +23,8 @@ onready var control_count : int
 
 onready var timer_timeout : Dictionary = {}
 
+onready var timer_expire : Dictionary = {}
+
 onready var sequence : Array = []
 
 onready var current_control : Array = []
@@ -59,24 +61,47 @@ func _ready():
 					state.timeout_route[i["Timeout"]] = target_id
 				if "Fire" in i:
 					state.fire[i["Control"]] = i["Fire"]
+				if "Timer" in i:
+					state.timer_reset[i["Control"]].append(i["Timer"])
 			states.append(state)
-
-func activate_sequence(_sequence_id : int, _delay : int, _cooldown : int) -> bool:
-	return false
-
-func process_move() ->void:
-	pass
+	set_process(true)
 
 # Entry point : Set the current control change.
 func set_move(control : int, pressed : bool) -> void:
 	# TODO Filter
 	current_control[control] = pressed
+	for timer in states[current_state].timer_reset:
+		var current_time : int = OS.get_current_msec()
+		timer_expire[timer] = current_time + timer_timeout[timer]
 	var seq_id : int = states[current_state].fire[control]
 	var override_sequence : bool = true
 	if pressed and seq_id >= 0:
 		override_sequence = activate_sequence(seq_id, sequence[seq_id].delay, sequence[seq_id].cooldown)
 
 	if override_sequence:
-		process_move()
+		process_move(control, pressed)
 
 	current_state = states[current_state].access[current_state]
+
+func _process(_delta : float):
+	var current_time : int = OS.get_current_msec()
+	for i in timer_expire:
+		if timer_expire[i] < 0:
+			continue
+		if timer_expire[i] <= current_time:
+			timer_expire[i] = -1
+		if i in states[current_state].timeout_route:
+			var next_state : int = states[current_state].timeout_route[i]
+			on_timer_expire(i)
+			current_state = next_state
+
+## "Virtual Methods"
+
+func activate_sequence(_sequence_id : int, _delay : int, _cooldown : int) -> bool:
+	return false
+
+func process_move(_control : int, _pressed : bool) -> void:
+	pass
+
+func on_timer_expire(timer : String) -> void:
+	pass
